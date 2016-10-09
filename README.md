62259b7bb434c7837b5ec0499577327cbe415a12
Jacek Tomaszewski <jacek@jtom.me>
2016-10-09 18:45:53 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,68 @@
+# HotApp.js
+
+Simple wrapper to make your js app start, stop and restart easily through the browser console or f.e. webpack hot reload system.
+
+# Usage example: Webpack, hot reload, angular1 with angular2 upgrade
+
+```ts
+// -- helpers
+function requireAll(r) { r.keys().forEach(r); }
+
+// define the ng1 app module, only if not defined yet
+if (!window.__ng1AppDefined) {
+  angular.module("app", []);
+}
+window.__ng1AppDefined = true;
+
+// Require the ng1 app code: its' directives, components, services code and so on
+//
+// NOTE Your ng1 code should use angular-hot library
+//      (https://github.com/jtomaszewski/angular-hot)
+//      If not, propably it will raise error while redefining the ng1 directives/components.
+requireAll(require.context('./ng1/', true, /\.js$/));
+
+// Require the ng2 app code: the ngmodule and all its requires should be in upgrade_adapter.ts code
+import { adapter } from './upgrade_adapter';
+
+// Now, let's use the HotApp to run the app and eventually restart it, if needed
+
+import { HotApp } from 'hot-app';
+
+let app = (<any>window).app = new HotApp({
+  oldApp: (<any>window).app,
+  rootElement: document.body,
+  startFn: (app, onStart) => {
+    app.ng2 = adapter.bootstrap(app.rootElement, ['app'], {strictDi: false});
+    app.ng2.ready(() => {
+      onStart();
+    });
+  },
+  stopFn: (app, onStop) => {
+    app.ng2.dispose();
+    onStop();
+  }
+});
+
+app.startOnDOMReady();
+
+// Webpack hot reload support
+if (module.hot) {
+  module.hot.accept();
+
+  module.hot.dispose(() => {
+    app.stop();
+  });
+}
+```
+
+# Usage example: Webpack, hot reload, angular2
+```ts
+```
+
+# License
+
+MIT
+
+# Contribution, Bug reports, Questions
+
+Just use github issues or pull requests! ;)
---
+++ hot-app.js
@@ -0,0 +1,73 @@
+;(function(factory) {
+  if (typeof module !== 'undefined' && module.exports) {
+    // Node/CommonJS
+    module.exports = factory(this);
+  } else if (typeof define === 'function' && define.amd) {
+    // AMD
+    var global=this;
+    define('hot-app', function(){ return factory(global);});
+  } else {
+    // Browser globals
+    this.HotApp = factory(this);
+  }
+}(function(global) {
+  "use strict";
+
+  // Options:
+  // - oldApp: HotApp|null - remember to pass it, if it already exists
+  // - rootElement: HTMLElement - f.e. `document.body`
+  // - startFn: Function(app: HotApp, onStart: Function)
+  // - stopFn: Function(app: HotApp, onStop: Function)
+  function HotApp(options) {
+    this.rootElement = options.rootElement;
+    this.startFn = options.startFn;
+    this.stopFn = options.stopFn;
+    if (options.oldApp) {
+      this.rootElementHtml = options.oldApp.rootElementHtml;
+    }
+
+    this._isRunning = false;
+  }
+
+  HotApp.prototype.isRunning = function() {
+    return this._isRunning;
+  }
+
+  HotApp.prototype.start = function(afterStart) {
+    // If rootElementHtml is given, let's replace the rootElement's html then
+    if (this.rootElementHtml) {
+      this.rootElement.outerHTML = this.rootElementHtml;
+    // If not, let's bind it to this app, so it can be used for the "new" app
+    } else {
+      this.rootElementHtml = this.rootElement.outerHTML;
+    }
+
+    var _this = this;
+    this.startFn(this, function() {
+      _this._isRunning = true;
+      console.debug('app has been started.');
+      afterStart && afterStart(_this);
+    });
+  }
+
+  HotApp.prototype.stop = function(afterStop) {
+    var _this = this;
+    this.stopFn(this, function() {
+      _this._isRunning = false;
+      console.debug('app has been stopped.');
+      afterStop && afterStop(_this);
+    });
+  }
+
+  HotApp.prototype.restart = function(afterRestart) {
+    console.debug('app restarting...');
+    var _this = this;
+    this.stop(function() {
+      _this.start(function() {
+        afterRestart && afterRestart(_this);
+      });
+    });
+  }
+
+  return HotApp;
+}));
